e75304a9fa71501375a4966d112e04c01cd24711
ricardoalves1 <ras@ic.ufal.br>
2019-03-15 20:50:26 -0300

    commit

--- fisica-projeto/scripts.js
+++ fisica-projeto/scripts.js
@@ -1,3 +1,36 @@
-function redirect(string) {
-    window.location.href = string + ".html";
+function show(m) {
+    if (document.getElementById('movimento').src.split("/").slice(-1)[0] == m) {
+        document.getElementById('movimento').src = '';
+        document.getElementById('iframe').style.display = 'none';
+    }
+    else {
+        document.getElementById('movimento').src = m;
+        document.getElementById('iframe').style.display = 'block';
+       	resizeIframe();
+    }
+}
+
+function resizeIframe() {
+
+	var obj = document.getElementById('movimento');
+	var width = screen.width;
+	var height = screen.height;
+	var size = width < height ? width : height;
+
+	if (obj.contentWindow.document.body.scrollHeight == 150) {
+		obj.style.height = (obj.contentWindow.document.body.scrollHeight + size) + 'px';
+	}
+	else {
+		obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+	}
+
+    
+}
+
+function resize(id) {
+	var width = screen.width;
+	var height = screen.height;
+	var size = width < height ? width : height;
+	document.getElementById(id).style.width = size+"px";
+	document.getElementById(id).style.height = size+"px";
 }
